a62aa35e05fbbd67b569c5919dc99c6203ada239
Larr014 <larr014@gmail.com>
2022-10-27 17:31:11 -0300

    Clase 10-27
    a

---
+++ app/src/main/java/com/example/appmovilb50/Modelo/Alumno.java
@@ -0,0 +1,20 @@
+package com.example.appmovilb50.Modelo;
+
+public class Alumno {
+    public String key;
+    public String rut;
+    public String nombre;
+    public String apellido;
+    public int edad;
+
+    public Alumno() {
+    }
+
+    public Alumno(String key, String rut, String nombre, String apellido, int edad) {
+        this.key = key;
+        this.rut = rut;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+}
